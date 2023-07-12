f1ea6d59c180857f1ecd6d5b652b89cf796f7940
Anastasia Belova <nastasya.belowa@yandex.ru>
2023-07-12 17:44:31 +0300

    работает

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,41 +1,97 @@
 const Card = require('../models/card');
 
+// получение всех карточек
 module.exports.getAllCards = (req, res) => {
   Card.find({})
-    .then((cards) => res.send(cards))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((cards) => res.status(200).send(cards))
+    .catch((err) =>
+      res.status(500).send({ message: 'Ошибка на стороне сервера', err })
+    );
 };
 
+// создание карточки
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   const owner = req.user._id;
   Card.create({ name, link, owner })
-    .then((card) => res.send(card))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((card) => res.status(201).send(card))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res
+          .status(400)
+          .send({ message: 'Переданы некорректные данные' });
+      }
+      return res
+        .status(500)
+        .send({ message: 'Ошибка на стороне сервера', err });
+    });
 };
 
+// удаление карточки
 module.exports.deleteCardById = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .then((card) => res.send(card))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .orFail()
+    .then((card) => res.status(201).send(card))
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        return res
+          .status(404)
+          .send({ message: 'Такой карточки не существует' });
+      }
+      return res
+        .status(500)
+        .send({ message: 'Ошибка на стороне сервера', err });
+    });
 };
 
+// лайк
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
     { new: true }
   )
-    .then((card) => res.send(card))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .orFail()
+    .then((card) => res.status(201).send(card))
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        return res
+          .status(404)
+          .send({ message: 'Такой карточки не существует' });
+      }
+      if (err.name === 'CastError') {
+        return res
+          .status(400)
+          .send({ message: 'Переданы некорректные данные' });
+      }
+      return res
+        .status(500)
+        .send({ message: 'Ошибка на стороне сервера', err });
+    });
 };
 
+// снятие лайка
 module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } },
     { new: true }
   )
-    .then((card) => res.send(card))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .orFail()
+    .then((card) => res.status(201).send(card))
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        return res
+          .status(404)
+          .send({ message: 'Такой карточки не существует' });
+      }
+      if (err.name === 'CastError') {
+        return res
+          .status(400)
+          .send({ message: 'Переданы некорректные данные' });
+      }
+      return res
+        .status(500)
+        .send({ message: 'Ошибка на стороне сервера', err });
+    });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -3,42 +3,89 @@ const User = require('../models/user');
 // список всех пользователей
 module.exports.getAllUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send(users))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((users) => res.status(200).send(users))
+    .catch((err) =>
+      res.status(500).send({ message: 'Ошибка на стороне сервера', err })
+    );
 };
 
 // поиск конкретного пользователя
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.userId)
-    .then((user) => {
-      if (!user) {
-        res.status(400).send({ message: 'Такого пользователя не существует' });
+    .orFail()
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        return res
+          .status(404)
+          .send({ message: 'Такого пользователя не существует' });
       }
-      res.status(200).send(user);
-    })
-    .catch((err) => res.status(500).send({ message: err.message }));
+      return res
+        .status(500)
+        .send({ message: 'Ошибка на стороне сервера', err });
+    });
 };
 
 // создание пользователя
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then((user) => res.send(user))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .orFail()
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res
+          .status(400)
+          .send({ message: 'Переданы некорректные данные пользователя' });
+      }
+      return res
+        .status(500)
+        .send({ message: 'Ошибка на стороне сервера', err });
+    });
 };
 
 // изменение профиля пользователя
 module.exports.updateUserProfile = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
-    .then((user) => res.send(user))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .orFail()
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res
+          .status(400)
+          .send({ message: 'Переданы некорректные данные пользователя' });
+      }
+      if (err.name === 'DocumentNotFoundError') {
+        return res
+          .status(404)
+          .send({ message: 'Такого пользователя не существует' });
+      }
+      return res
+        .status(500)
+        .send({ message: 'Ошибка на стороне сервера', err });
+    });
 };
 
 // изменение аватара пользователя
 module.exports.updateUserAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
-    .then((user) => res.send(user))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .orFail()
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res
+          .status(400)
+          .send({ message: 'Переданы некорректные данные пользователя' });
+      }
+      if (err.name === 'DocumentNotFoundError') {
+        return res
+          .status(404)
+          .send({ message: 'Такого пользователя не существует' });
+      }
+      return res
+        .status(500)
+        .send({ message: 'Ошибка на стороне сервера', err });
+    });
 };
