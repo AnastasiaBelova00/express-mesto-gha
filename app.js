0d01d4786073cd926a6d1f709ce5de74af174344
Anastasia Belova <nastasya.belowa@yandex.ru>
2023-07-12 12:56:06 +0300

    новый хост

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cardRoute = require('./routes/cards');
 const app = express();
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use((req, res, next) => {
   req.user = {
