b4187aa9a0c957eff66f8e636277afc02b7811ac
Anastasia Belova <nastasya.belowa@yandex.ru>
2023-07-25 18:31:19 +0300

    regexp

--- app.js
+++ app.js
@@ -46,10 +46,8 @@ app.post(
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().pattern(
-        new RegExp(
-          '/^(http(s?)://)?(www.)+[a-zA-Z0-9.-_]+(.[a-zA-Z]{2,3})+(/[a-zA-Z0-9_-s./?%#&=]*)?$/'
-        )
+      avatar: Joi.string().regex(
+        '/^(http(s?)://)?(www.)+[a-zA-Z0-9.-_]+(.[a-zA-Z]{2,3})+(/[a-zA-Z0-9_-s./?%#&=]*)?$/'
       ),
       email: Joi.string().required().email(),
       password: Joi.string().required().min(8),
--- routes/users.js
+++ routes/users.js
@@ -19,8 +19,8 @@ router.patch(
   '/me',
   celebrate({
     body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
+      name: Joi.string().required().min(2).max(30),
+      about: Joi.string().required().min(2).max(30),
     }),
   }),
   updateUserProfile
@@ -30,11 +30,11 @@ router.patch(
   '/me/avatar',
   celebrate({
     body: Joi.object().keys({
-      avatar: Joi.string().pattern(
-        new RegExp(
+      avatar: Joi.string()
+        .required()
+        .regex(
           '/^(http(s?)://)?(www.)+[a-zA-Z0-9.-_]+(.[a-zA-Z]{2,3})+(/[a-zA-Z0-9_-s./?%#&=]*)?$/'
-        )
-      ),
+        ),
     }),
   }),
   updateUserAvatar
